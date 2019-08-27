b83cc41a663304d2feee8283859319d7f906f9f9
Wendy Garcia <wendy.fern84@gmail.com>
2019-08-26 21:30:29 -0700

    added conditionals to comper user guess with random letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,32 @@
 // Randomly pick a letter 
 var abc = "abcdefghijklmnopqrstuvwxyz";
 var randomLetter = abc[Math.floor(Math.random()*26)];
-console.log(randomLetter);
+console.log(randomLetter); // SANITY CHECK
 
 var guessesSoFar = [];
 var wins = 0;
 var loses = 0;
 var guessesLeft = 10;
 
+do {}
 // Prompt user for a guess
 var userGuess = prompt("Guess what letter I'm thinking of");
 
-// Concatenate letter in "your guesses, so far" string, and display the whole string
-guessesSoFar.push(userGuess);
-console.log(userGuess);
+// Concatenate letter in "your guesses, so far" string
+guessesSoFar.push(userGuess); 
+console.log(userGuess); //SANITY CHECK
+
+//and display the whole string
+
 // Compare computer's choice with user's choice. Are they equal??
-            //Increase wins variable and decrease guesses, restart the game w/o refreshing page
-// If not equal
-            // Increase number of Losses and decrease guesses, restart the game w/o refreshing
+if (randomLetter == userGuess.toLowerCase()) {
+    //Increase wins variable and , 
+    wins++;
+} else {
+    // Increase number of Losses and decrease guesses
+    loses--;
+}
+// decrease guesses regardless of wining or losing
+    guessesLeft--;
+
+    //restart the game w/o refreshing page until guessesLeft = 0
