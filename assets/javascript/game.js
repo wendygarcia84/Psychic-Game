ab5cc0b9e775f85efc717fecf97557c309df83ee
Wendy Garcia <wendy.fern84@gmail.com>
2019-08-27 10:01:38 -0700

    updating the number of losses and wins to the html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,44 @@
 // Randomly pick a letter 
 var abc = "abcdefghijklmnopqrstuvwxyz";
-var randomLetter = abc[Math.floor(Math.random()*26)];
-console.log(randomLetter); // SANITY CHECK
 
 var guessesSoFar = [];
-var wins = 0;
-var loses = 0;
-var guessesLeft = 10;
+var winCount = 0;
+var lossCount = 0;
+var guessesCount = 10;
 
 
-// Prompt user for a guess
-var userGuess = prompt("Guess what letter I'm thinking of");
+//Store the ID's of each span into a variable
+var userGuess = document.getElementById("guesses-so-far");
+var wins = document.getElementById("wins");
+var loses = document.getElementById("loses");
+document.getElementById("guesses-left");
 
-// Concatenate letter in "your guesses, so far" string
-guessesSoFar.push(userGuess); 
-console.log(userGuess); //SANITY CHECK
+// Next, we give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function(event) {
+            userGuess.textContent = event.key;
+            var randomLetter = abc[Math.floor(Math.random()*26)];
+                //and display the whole string
+                // Concatenate letter in "your guesses, so far" string
+            guessesSoFar.push(userGuess); 
 
-//and display the whole string
+            // Compare computer's choice with user's choice. Are they equal??
+            if (randomLetter == userGuess) {
+                //Increase wins variable and , 
+                winCount++;
+                document.getElementById('wins').innerHTML = winCount;
+            } else {
+                // Increase number of Losses and decrease guesses
+                lossCount++;
+                document.getElementById('losses').innerHTML = lossCount;
+            }
+            // decrease guesses regardless of wining or losing
+                guessesCount--;
+                console.log(randomLetter); // SANITY CHECK
+                console.log("Guesses left: " + guessesCount);
+                document.getElementById('guesses-left').innerHTML = guessesCount;
 
-// Compare computer's choice with user's choice. Are they equal??
-if (randomLetter == userGuess.toLowerCase()) {
-    //Increase wins variable and , 
-    wins++;
-} else {
-    // Increase number of Losses and decrease guesses
-    loses++;
-}
-// decrease guesses regardless of wining or losing
-    guessesLeft--;
+                //restart the game w/o refreshing page until guessesLeft = 0
+};
 
-    //restart the game w/o refreshing page until guessesLeft = 0
+
+
