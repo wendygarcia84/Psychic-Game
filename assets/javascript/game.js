c020927f3e00888453ee2aeaf084de63bd39733d
Wendy Garcia <wendy.fern84@gmail.com>
2019-08-27 21:37:38 -0700

    Fixing problems with logic, conditions, and how scores were updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,39 +5,42 @@ var guessesSoFar = [];
 var winCount = 0;
 var lossCount = 0;
 var guessesCount = 10;
+var userGuess;
 
 
 //Store the ID's of each span into a variable
-var userGuess = document.getElementById("guesses-so-far");
-var wins = document.getElementById("wins");
-var loses = document.getElementById("loses");
+ //var guessesText = document.getElementById("guesses-so-far");
+// var wins = document.getElementById("wins");
+// var losses = document.getElementById("losses");
 document.getElementById("guesses-left");
 
 // Next, we give JavaScript a function to execute when onkeyup event fires.
 document.onkeyup = function(event) {
-            userGuess.textContent = event.key;
+            userGuess = event.key;
             var randomLetter = abc[Math.floor(Math.random()*26)];
                 //and display the whole string
-                // Concatenate letter in "your guesses, so far" string
-            guessesSoFar.push(userGuess); 
-
+                // Concatenate letter in "your guesses, so far" string 
+            guessesSoFar.push(userGuess);
             // Compare computer's choice with user's choice. Are they equal??
-            if (randomLetter == userGuess) {
+            if (randomLetter === userGuess) {
                 //Increase wins variable and , 
                 winCount++;
-                document.getElementById('wins').innerHTML = winCount;
+                guessesCount = 10;
+                guessesSoFar = [];
             } else {
-                // Increase number of Losses and decrease guesses
-                lossCount++;
-                document.getElementById('losses').innerHTML = lossCount;
-            }
             // decrease guesses regardless of wining or losing
                 guessesCount--;
-                console.log(randomLetter); // SANITY CHECK
-                console.log("Guesses left: " + guessesCount);
-                document.getElementById('guesses-left').innerHTML = guessesCount;
-
+            }
                 //restart the game w/o refreshing page until guessesLeft = 0
+            if (guessesCount === 0) {
+                lossCount++;
+                guessesCount = 10;
+                guessesSoFar = [];
+            }
+            document.getElementById('wins').innerHTML = winCount;
+            document.getElementById('losses').innerHTML = lossCount;
+            document.getElementById('guesses-left').innerHTML = guessesCount;
+            document.getElementById('guesses-so-far').innerHTML = guessesSoFar;
 };
 
 
